a0dd3c126625b815439201994e8a3d40a702ad9b
Ikechukwu Eze <92305839+ikezedev@users.noreply.github.com>
2024-11-28 01:10:27 +0000

    flesh out Rust client

--- examples/cog-idp-iam-sync/iam-syncing-client/src/main.rs
+++ examples/cog-idp-iam-sync/iam-syncing-client/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use cog_idp_syncer::sync_request::Body;
+use cog_idp_syncer::sync_response::Body as ResponseBody;
+
+use cog_idp_syncer::cog_idp_to_gandalf_syncer_client::CogIdpToGandalfSyncerClient;
+use cog_idp_syncer::{ProjectsRequest, SyncRequest};
+
+pub mod cog_idp_syncer {
+    tonic::include_proto!("cog_idp_syncer");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = CogIdpToGandalfSyncerClient::connect("http://[::1]:50051").await?;
+
+    let request = tonic::Request::new(SyncRequest {
+        body: Some(Body::Projects(ProjectsRequest { cursor: None })),
+    });
+
+    let request = client.sync(request).await?;
+
+    let mut inbound = request.into_inner();
+
+    while let Some(response) = inbound.message().await? {
+        println!("RESPONSE={:?}", response);
+        match response.body {
+            Some(ResponseBody::Project(project_sync_response)) => {
+                println!("PROJECT RESPONSE={:?}", project_sync_response);
+            }
+            Some(ResponseBody::Token(_)) => unreachable!(),
+            None => todo!(),
+        }
+    }
+
+    Ok(())
 }
